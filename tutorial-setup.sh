b8456913b2715ee1013fa9256c26a084b36d06e7
Daniel Morales <79028039+dmora127@users.noreply.github.com>
2025-10-28 12:08:00 -0500

    Add username parameter to tutorial setup script

--- tutorial-setup.sh
+++ tutorial-setup.sh
@@ -3,6 +3,8 @@
 # Tutorial setup script to prepare the environment for running the tutorial
 # Usage: ./tutorial-setup.sh <username>
 
+MyUSERNAME="$1"
+
 set -euo pipefail
 
 mkdir -p ospool/ap40/data/$MyUSERNAME/tutorial-ONT-Basecalling
